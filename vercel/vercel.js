aaa084300ab8a20c2617e87fccbae13f47f56864
CHINAupup <42591047+CHINAupup@users.noreply.github.com>
2023-11-23 11:51:14 +0800

    Update vercel.js

--- vercel/vercel.js
+++ vercel/vercel.js
@@ -1,30 +1,6 @@
-const https = require('https');
-
 module.exports = (req, res) => {
-    const options = {
-        hostname: 'raw.githubusercontent.com',
-        port: 443,
-        path: '/CHINAupup/proxy-go-merge/main/sub/merged_proxies_new.yaml',
-        method: 'GET'
-    };
-
-    const request = https.request(options, response => {
-        let data = '';
-
-        response.on('data', chunk => {
-            data += chunk;
-        });
-
-        response.on('end', () => {
-            res.setHeader('Content-Type', 'text/plain');
-            res.status(200).send(data);
-        });
+    res.writeHead(302, {
+        'Location': 'https://raw.githubusercontent.com/CHINAupup/proxy-go-merge/main/sub/merged_proxies.yaml'
     });
-
-    request.on('error', error => {
-        console.error(error);
-        res.status(500).send('An error occurred.');
-    });
-
-    request.end();
+    res.end();
 };
