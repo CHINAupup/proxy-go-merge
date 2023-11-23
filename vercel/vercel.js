971beb5300596eddebe8b1e4f58b0fee5afa2bbb
CHINAupup <42591047+CHINAupup@users.noreply.github.com>
2023-11-23 12:00:07 +0800

    Update and rename vercel/vercel.js to vercel.json

--- vercel/vercel.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-    res.writeHead(302, {
-        'Location': 'https://raw.githubusercontent.com/CHINAupup/proxy-go-merge/main/sub/merged_proxies.yaml'
-    });
-    res.end();
-};
